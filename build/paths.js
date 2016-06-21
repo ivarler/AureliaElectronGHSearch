b5b81cb823e78022b865cfbbc870a01e566f2c8b
Bernhard Richter <bernhard.richter@gmail.com>
2016-06-21 14:25:08 +0200

    Initial commit

---
+++ build/paths.js
@@ -0,0 +1,21 @@
+var appRoot = 'src/';
+var outputRoot = 'dist/';
+var exporSrvtRoot = 'export/'
+
+module.exports = {
+  root: appRoot,
+  source: appRoot + '**/*.ts',
+  html: appRoot + '**/*.html',
+  css: appRoot + '**/*.css',
+  style: 'styles/**/*.css',
+  output: outputRoot,
+  exportSrv: exporSrvtRoot,
+  doc: './doc',
+  e2eSpecsSrc: 'test/e2e/src/**/*.ts',
+  e2eSpecsDist: 'test/e2e/dist/',
+  dtsSrc: [
+    './typings/globals/**/*.d.ts',
+    './custom_typings/**/*.d.ts',
+    './jspm_packages/**/*.d.ts'
+  ]
+}
